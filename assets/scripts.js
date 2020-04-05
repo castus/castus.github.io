0ccd875f72a74f2175aa5aa10ddfbb924459e1b7
Krzysztof Romanowski <castus.pl@gmail.com>
2020-04-05 15:26:36 +0200

    Replace icons with FontsAwesome

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,11 +6,13 @@
 
     var pattern = Trianglify({
       height: 300,
-      width: window.innerWidth,
+      width: document.documentElement.clientWidth,
       cell_size: 50,
       x_colors: 'YlGnBu'
     });
 
+    var elem = document.querySelector(".site-header canvas");
+    elem.parentNode.removeChild(elem);
     document.querySelector(".site-header").appendChild(pattern.canvas());
   }
   resizeCanvas();
